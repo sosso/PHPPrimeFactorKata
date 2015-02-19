00f5600fb7262349ce8c18cc6a974f1cfbe99d2b
Anthony Sosso <asosso@findthebest.com>
2015-02-18 20:07:42 -0800

    5: just works

--- spec/PrimeFactorizerSpec.php
+++ spec/PrimeFactorizerSpec.php
@@ -23,4 +23,7 @@ class PrimeFactorizerSpec extends ObjectBehavior {
 	function it_returns_2_2_for_4() {
 		$this->generate(4)->shouldReturn([2,2]);
 	}
+	function it_returns_5_for_5() {
+		$this->generate(5)->shouldReturn([5]);
+	}
 }
