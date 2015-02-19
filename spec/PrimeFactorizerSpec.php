3ca48ee16cf63c33f876216e4af42bc9e159084d
sosso <xtremerunnerars@gmail.com>
2015-02-18 19:42:51 -0800

    Update PrimeFactorizerSpec.php

--- spec/PrimeFactorizerSpec.php
+++ spec/PrimeFactorizerSpec.php
@@ -7,7 +7,7 @@ use Prophecy\Argument;
 
 class PrimeFactorizerSpec extends ObjectBehavior {
     function it_is_initializable() {
-        $this->shouldHaveType('PrimeFactorizerSpec');
+        $this->shouldHaveType('PrimeFactorizer');
     }
 
 	function it_returns_empty_arr_for_1() {
