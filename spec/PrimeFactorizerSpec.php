04020d8bfc7e13f682ccbc875f04568b2983ab16
Anthony Sosso <asosso@findthebest.com>
2015-02-18 19:24:34 -0800

    Base state

---
+++ spec/PrimeFactorizerSpec.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PrimeFactorizerSpec extends ObjectBehavior {
+    function it_is_initializable() {
+        $this->shouldHaveType('PrimeFactorizerSpec');
+    }
+
+	function it_returns_empty_arr_for_1() {
+
+	}
+}
